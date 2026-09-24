05fb172ab93112ea67cc659adaf981236e252843
Vangie Du <duwan@live.com>
2026-09-24 22:40:04 +0800

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        const n = temperatures.length;
+        const res = new Array(n).fill(0);
+
+        for (let i = n - 2; i >= 0; i--) {
+            let j = i + 1;
+            while (j < n && temperatures[j] <= temperatures[i]) {
+                if (res[j] === 0) {
+                    j = n;
+                    break;
+                }
+                j += res[j];
+            }
+
+            if(j<n){
+                res[i] = j-i;
+            }
+        }
+        return res;
+    }
+}
